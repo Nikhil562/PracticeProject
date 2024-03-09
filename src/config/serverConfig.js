4c5fe2a4abecafdf326ea27afaba3356781aa775
bhaiyu <niksharma34@gmail.com>
2024-03-10 04:41:45 +0530

    basic dotenv file created

---
+++ src/config/serverConfig.js
@@ -0,0 +1,5 @@
+const dotnev=require("dotenv");
+dotnev.config();
+module.exports={
+    PORT:process.env.PORT
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-const PORT=
-app.listen(3000,()=>{
-    console.log('connected ')
+const {PORT}=require('./config/serverConfig');
+app.listen(PORT,()=>{
+    console.log(`Connection setup at port no ${PORT}`)
 })
